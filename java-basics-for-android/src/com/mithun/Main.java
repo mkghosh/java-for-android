70d46d7e380eedbb351c8dbcf8f055d555243e63
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-10 20:44:15 +0600

    added Grade class in conditionals.

---
+++ java-basics-for-android/src/com/mithun/Main.java
@@ -0,0 +1,7 @@
+package com.mithun;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am the test class for this java project.");
+    }
+}
---
+++ java-basics-for-android/src/com/mithun/conditionals/Grade.java
@@ -0,0 +1,20 @@
+package com.mithun.conditionals;
+
+/**
+ * <h1>This Class is responsible for calculating students grade bye providing the obtained mark.</h1>
+ * Created by Mithun Kumer Ghose on 4/10/2016.
+ */
+public class Grade {
+
+    private enum CharConstant{
+
+    }
+    /**
+     * <h1>By using this method we can calculate a single students single subject's grade</h1>
+     * @param markObtained {@link Integer} the mark obtained by the student.
+     * @return {@link Character} The student's grade in char representation.
+     */
+    public static char getGrade(int markObtained) {
+        return 0;
+    }
+}
