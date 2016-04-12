8893b7b03e554677daab64ac878fc42d56ecf364
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-12 20:56:02 +0600

    Added MarkProcessor class

--- java-basics-for-android/src/com/mithun/Main.java
+++ java-basics-for-android/src/com/mithun/Main.java
@@ -1,10 +1,16 @@
 package com.mithun;
 
+import com.mithun.arrays.MarkProcessor;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Please provide students mark separated by space.");
+        String givenMarks = scanner.nextLine();
+        MarkProcessor markProcessor = new MarkProcessor();
+        double average = markProcessor.getAverageMark(givenMarks);
+        System.out.println("The average mark of students is : " + average);
     }
 }
