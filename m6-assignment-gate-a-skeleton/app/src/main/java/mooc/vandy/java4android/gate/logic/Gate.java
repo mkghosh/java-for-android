d91c08f49181f9646804ca56f23908f02f44bf63
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-14 00:34:48 +0600

    added all the methods in gate class

--- m6-assignment-gate-a-skeleton/app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
+++ m6-assignment-gate-a-skeleton/app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -4,7 +4,29 @@ package mooc.vandy.java4android.gate.logic;
  * @@ Julie, please fill in here.
  */
 public class Gate {
-  
+    public static final Object OUT = null;
+    public static final Object IN = null;
+
+    public boolean isLocked() {
+        return false;
+    }
+
+    public String getSwingDirection() {
+        return null;
+    }
+
+    public void close() {
+
+    }
+
+    public void open(Object out) {
+
+    }
+
+    public void setSwing(Object in) {
+
+    }
+
     // TODO -- Fill in your code here
   
 }
