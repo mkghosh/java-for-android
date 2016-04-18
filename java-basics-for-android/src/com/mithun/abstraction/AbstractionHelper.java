1d1eccb1dc6e96ca5af6de52ff8a2962ff4c3da0
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-18 08:13:32 +0600

    Updated abstractionHelper

--- java-basics-for-android/src/com/mithun/abstraction/AbstractionHelper.java
+++ java-basics-for-android/src/com/mithun/abstraction/AbstractionHelper.java
@@ -1,8 +1,9 @@
 package com.mithun.abstraction;
 
 /**
+ * <h1>This abstract class will be the parent of all other classes is this repository and will have all the common task of those classes.</h1>
  * Created by Mithun Kumer Ghose on 4/9/2016.
  */
-public class AbstractionHelper {
+public abstract class AbstractionHelper {
 
 }
