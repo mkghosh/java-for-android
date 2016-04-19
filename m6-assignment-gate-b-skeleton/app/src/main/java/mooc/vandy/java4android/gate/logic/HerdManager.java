1f74eaec004b522b568861c0ad2b799fd1f31884
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-19 20:14:54 +0600

    updated herdmanager with proper comment.

--- m6-assignment-gate-b-skeleton/app/src/main/java/mooc/vandy/java4android/gate/logic/HerdManager.java
+++ m6-assignment-gate-b-skeleton/app/src/main/java/mooc/vandy/java4android/gate/logic/HerdManager.java
@@ -32,11 +32,18 @@ public class HerdManager {
     }
 
     // TODO -- Fill your code in here
+
+    /**
+     * <h1>This method Sets the {@link Gate} of the Pen.</h1>
+     * <p>If the {@link Gate} is westGate that means it is to enter the snails in the pen.</p>
+     * <p>If the {@link Gate} is eastGate that means it is to enter the snails in the pasture</p>
+     * @param westGate {@link Gate} for entering the Pen.
+     * @param eastGate {@link Gate} for entering the Pasture.
+     */
     public void setGates(Gate westGate, Gate eastGate) {
 
         //seting the gates swing values
         westGate.setSwing(Gate.IN);
-
         eastGate.setSwing(Gate.OUT);
 
         //seting the gates to the instance variables
@@ -45,21 +52,38 @@ public class HerdManager {
 
     }
 
+    /**
+     * <h1>This method is for creating the simulation of the snails to go to and from the pen</h1>
+     * @param westGate {@link Gate} for entering the Pen.
+     * @param eastGate {@link Gate} for entering the Pasture.
+     * @param random {@link Random} for selecting the {@link Gate} and selecting the number of snails to thru.
+     */
     public void simulateHerd(Gate westGate, Gate eastGate, Random random) {
+
+        //Setting the number of snails in Pen equals to the number of total herd.
         int inPen = HERD;
+
+        //Setting the number of snails in the pasture to zero.
         int inPasture = 0;
 
+        //printing out the current value of the snails in pen and pasture.
         mOut.print(inPen + " ");
         mOut.print(inPasture + " ");
+
+        //initializing the Gate with null value
         Gate gate = null;
+
+        //Creating a for loop to loop for 10 times.
         for(int i = 0; i < 10; i++) {
 
+            //setting the gate's value to the desired gate
             if(inPasture == 0) {
                 gate = eastGate;
             } else {
                 gate = random.nextBoolean() ? eastGate : westGate;
             }
 
+            //Moving the snails according to the selection of the gate.
             if(gate.getSwingDirection() == Gate.IN) {
                 int numberOfSnailToMove = random.nextInt(inPasture) + 1;
                 inPasture -= numberOfSnailToMove;
