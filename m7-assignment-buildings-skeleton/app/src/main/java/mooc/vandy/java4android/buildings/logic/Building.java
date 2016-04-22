62a16c18d34af4fe12a092706ae83ee011b44b8e
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-22 08:07:11 +0600

    Buildings instance variables, basic methods and constructor are created

--- m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/Building.java
+++ m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/Building.java
@@ -4,5 +4,61 @@ package mooc.vandy.java4android.buildings.logic;
  * This is the Building class file.
  */
 public class Building {
-    // TODO -- add your code here
+
+    private int length;
+    private int width;
+    private int lotLength;
+    private int lotWidth;
+
+    public Building(int	length, int	width, int	lotLength, int	lotWidth) {
+        setLength(length);
+        setWidth(width);
+        setLotLength(lotLength);
+        setLotWidth(lotWidth);
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    private void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    private void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLotLength() {
+        return lotLength;
+    }
+
+    private void setLotLength(int lotLength) {
+        this.lotLength = lotLength;
+    }
+
+    public int getLotWidth() {
+        return lotWidth;
+    }
+
+    private void setLotWidth(int lotWidth) {
+        this.lotWidth = lotWidth;
+    }
+
+    public int calcBuildingArea() {
+        return 0;
+    }
+
+    public int calcLotArea() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
