40758aa976663015e64fbf8cbb9c082640df53fd
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-22 08:11:38 +0600

    House instance variables, basic methods and constructors created.

--- m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
+++ m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
@@ -3,7 +3,45 @@ package mooc.vandy.java4android.buildings.logic;
 /**
  * This is the House class file that extends Building.
  */
-public class House 
-       extends Building {
-    // TODO -- add your code here
+public class House extends Building {
+
+    private String owner;
+    private boolean pool;
+
+    public House(int length, int width, int lotLength, int lotWidth) {
+        super(length, width, lotLength, lotWidth);
+    }
+
+    public House(int	length,	int	width,	int	lotLength,	int	lotWidth,	String	owner) {
+        this(length,width,lotLength,lotWidth);
+    }
+    public House(int	length,	int	width,	int	lotLength,	int	lotWidth,	String	owner, boolean pool) {
+        this(length,width,lotLength,lotWidth);
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public boolean isPool() {
+        return pool;
+    }
+
+    public void setPool(boolean pool) {
+        this.pool = pool;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
 }
