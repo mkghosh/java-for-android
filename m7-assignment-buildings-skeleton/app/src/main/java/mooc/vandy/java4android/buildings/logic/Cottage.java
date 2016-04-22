ee924c131f37f54b179473c3bb9f4ec3dc9b3189
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-22 08:28:09 +0600

    updated cottage class.

--- m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/Cottage.java
+++ m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/Cottage.java
@@ -3,8 +3,27 @@ package mooc.vandy.java4android.buildings.logic;
 /**
  * This is the cottage class file.  It is a subclass of House.
  */
-public class Cottage 
-       extends House {
-  // TODO -- add your code here
+public class Cottage extends House {
+
+    private boolean secondFloor;
+
+    public Cottage(int	dimension,	int	lotLength,	int	lotWidth) {
+        super(dimension, dimension, lotLength, lotWidth);
+    }
+
+    public Cottage(int	dimension,	int	lotLength,	int	lotWidth,	String	owner,	boolean
+            secondFloor) {
+        super(dimension, dimension, lotLength, lotWidth, owner);
+        this.secondFloor = secondFloor;
+    }
+
+    public boolean hasSecondFloor() {
+        return secondFloor;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
 
