df593e11098fbaea855e3191205c69d79c6b2db7
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-27 23:47:04 +0600

    AbstractClient extends AbstractionHelper.

--- java-basics-for-android/src/com/mithun/abstraction/AbstractClient.java
+++ java-basics-for-android/src/com/mithun/abstraction/AbstractClient.java
@@ -3,5 +3,10 @@ package com.mithun.abstraction;
 /**
  * Created by Mithun Kumer Ghose on 4/27/2016.
  */
-public class AbstractClient  {
+public class AbstractClient extends AbstractionHelper {
+
+    @Override
+    public void execute() {
+
+    }
 }
