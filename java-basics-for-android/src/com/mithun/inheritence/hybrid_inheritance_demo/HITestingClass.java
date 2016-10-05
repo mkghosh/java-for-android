09e298a4170a6f2f008369a4c4e96246a14b99a9
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-10-05 22:52:21 +0600

    Added classes in hybrid inheritance demo package

---
+++ java-basics-for-android/src/com/mithun/inheritence/hybrid_inheritance_demo/HITestingClass.java
@@ -0,0 +1,8 @@
+package com.mithun.inheritence.hybrid_inheritance_demo;
+
+/**
+ * @author Mithun Kumer Ghose.
+ */
+public class HITestingClass extends HIParentClass implements MyInterface {
+
+}
---
+++ java-basics-for-android/src/com/mithun/inheritence/hybrid_inheritance_demo/MyInterface.java
@@ -0,0 +1,7 @@
+package com.mithun.inheritence.hybrid_inheritance_demo;
+
+/**
+ * @author Mithun Kumer Ghose.
+ */
+public interface MyInterface {
+}
